963207018e97fc67a7307e1c2c6f05885edca914
deboralagemb <deboralagemb@gmail.com>
2021-04-19 01:56:59 +0000

    add get_type_id to interpreter, no validation yet

--- src/lib/interpreter.ml
+++ src/lib/interpreter.ml
@@ -1,1 +1,12 @@
 (* interpreter.ml *)
+
+open Absyn
+
+
+let get_type_id (type', id) =
+  match type' with
+  | Absyn.Int -> (id, Absyn.Int)
+  | Absyn.Bool -> (id, Absyn.Bool)
+
+(* let interpreter (_, y) = 
+  call_fun y *)
